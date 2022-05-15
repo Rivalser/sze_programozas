bd4db8b694ca5d12380aa5c38e8bfea07c7f2848
Rivalser <97056846+Rivalser@users.noreply.github.com>
2022-05-15 19:37:53 +0200

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n < 2)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+int main()
+{
+    int szam;
+    szam = fibonacci(11);
+    cout << szam;
+}
